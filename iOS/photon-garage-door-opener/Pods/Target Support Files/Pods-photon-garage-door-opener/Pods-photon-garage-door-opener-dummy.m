581090e6a03bed62edc2969f20300cffadb8ae01
Jasper Meurs <jasper@touching.be>
2015-10-04 16:44:59 +0200

    added basic operating functionality

---
+++ iOS/photon-garage-door-opener/Pods/Target Support Files/Pods-photon-garage-door-opener/Pods-photon-garage-door-opener-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_photon_garage_door_opener : NSObject
+@end
+@implementation PodsDummy_Pods_photon_garage_door_opener
+@end
--- iOS/photon-garage-door-opener/photon-garage-door-opener/ViewController.swift
+++ iOS/photon-garage-door-opener/photon-garage-door-opener/ViewController.swift
@@ -9,16 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var myPhoton : SparkDevice?
+    let particleConfig = ParticleConfiguration()
+    
+    override func viewWillAppear(animated: Bool) {
+        if SparkCloud.sharedInstance() == nil {
+            print("Nog instance created yet, logging in...")
+        SparkCloud.sharedInstance().loginWithUser(particleConfig.particleUsername, password: particleConfig.particlePassword) { (error:NSError!) -> Void in
+            if let _=error {
+                print("Wrong credentials!")
+            } else {
+                print("Logged in!")
+            }
+        }
+        }
+        
+        if myPhoton == nil {
+        SparkCloud.sharedInstance().getDevices { (sparkDevices:[AnyObject]!, error:NSError!) -> Void in
+            if let _=error
+            {
+                print("Check your internet connectivity")
+            }
+            else
+            {
+                if let devices = sparkDevices as? [SparkDevice]
+                {
+                    for device in devices
+                    {
+                        if device.name == self.particleConfig.particleTargetDevice
+                        {
+                            self.myPhoton = device
+                            print("Device found")
+                        }
+                        
+                    }
+                }
+            }
+        }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func operateGarageDoorButtonPressed() {
+        if myPhoton != nil {
+        let funcArgs = ["open"]
+        myPhoton!.callFunction("operateDoor", withArguments: funcArgs) { (resultCode : NSNumber!, error : NSError!) -> Void in
+            if (error == nil) {
+                print("The door is opening")
+            }
+        }
+        } else {
+            print("Particle not yet loaded")
+        }
+    }
+    
 
 
 }
