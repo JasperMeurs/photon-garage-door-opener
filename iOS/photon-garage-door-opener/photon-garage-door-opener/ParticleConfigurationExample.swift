8b312f1e3fc6c7c578c8e22006e2a0380352834e
Jasper Meurs <jasper@touching.be>
2015-10-04 16:49:01 +0200

    added example Particle configuration file

---
+++ iOS/photon-garage-door-opener/photon-garage-door-opener/ParticleConfigurationExample.swift
@@ -0,0 +1,15 @@
+//
+//  ParticleConfiguration.swift
+//  photon-garage-door-opener
+//
+//  Created by Jasper Meurs on 04/10/15.
+//  Copyright © 2015 Jasper Meurs. All rights reserved.
+//
+
+import Foundation
+
+struct ParticleConfiguration {
+    let particleUsername = "yourUsername"
+    let particlePassword = "yourPassword"
+    let particleTargetDevice = "yourDevice"
+}
