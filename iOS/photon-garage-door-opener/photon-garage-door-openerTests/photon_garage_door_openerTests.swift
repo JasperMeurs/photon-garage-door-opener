3e87e293b878486176fd538790522c498fea16c2
Jasper Meurs <jasper@touching.be>
2015-10-04 14:42:12 +0200

    initial commit

---
+++ iOS/photon-garage-door-opener/photon-garage-door-openerTests/photon_garage_door_openerTests.swift
@@ -0,0 +1,36 @@
+//
+//  photon_garage_door_openerTests.swift
+//  photon-garage-door-openerTests
+//
+//  Created by Jasper Meurs on 04/10/15.
+//  Copyright © 2015 Jasper Meurs. All rights reserved.
+//
+
+import XCTest
+@testable import photon_garage_door_opener
+
+class photon_garage_door_openerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
